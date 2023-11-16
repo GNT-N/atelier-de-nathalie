08b084350a6de2050f981ecd7c0138812661288a
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-11-16 12:46:16 +0100

    update form mobile

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -9,6 +9,8 @@ export default function Form() {
         message: "",
     });
 
+    const [validationMessage, setValidationMessage] = useState(null);
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
@@ -27,66 +29,88 @@ export default function Form() {
             });
 
             if (response.ok) {
-                console.log("E-mail envoyé avec succès.");
+                setValidationMessage({
+                    success: true,
+                    message: "E-mail envoyé avec succès.",
+                });
             } else {
-                console.error(
-                    "Erreur lors de l'envoi de l'e-mail. Statut :",
-                    response.status
-                );
-                const responseBody = await response.text();
-                console.error("Contenu de la réponse :", responseBody);
+                setValidationMessage({
+                    success: false,
+                    message: `Erreur lors de l'envoi de l'e-mail.`,
+                });
             }
         } catch (error) {
+            setValidationMessage({
+                success: false,
+                message: "Erreur lors de l'envoi de l'e-mail.",
+            });
             console.error("Erreur lors de l'envoi de l'e-mail :", error);
         }
     };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            method="post"
-            className={styles.customForm}
-        >
-            <div className={styles.customDiv}>
-                <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    placeholder="Prénom"
-                    className={styles.customInput}
-                />
-                <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    placeholder="Nom"
-                    className={styles.customInput}
-                />
-            </div>
-            <div>
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    className={styles.customInput}
-                />
-            </div>
-            <div>
-                <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    placeholder="Votre message"
-                    className={styles.customTextarea}
-                />
-            </div>
-            <button type="submit" className={styles.customSubmit}>
-                Envoyer
-            </button>
-        </form>
+        <div>
+            <form
+                onSubmit={handleSubmit}
+                method="post"
+                className={styles.customForm}
+            >
+                <div className={styles.customDiv}>
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        placeholder="Prénom"
+                        className={styles.customInput}
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        placeholder="Nom"
+                        className={styles.customInput}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="Email"
+                        className={styles.customInput}
+                        required
+                    />
+                </div>
+                <div>
+                    <textarea
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        placeholder="Votre message"
+                        className={styles.customTextarea}
+                        required
+                    />
+                </div>
+                <button type="submit" className={styles.customSubmit}>
+                    Envoyer
+                </button>
+            </form>
+            {validationMessage && (
+                <div
+                    className={
+                        validationMessage.success
+                            ? styles.successMessage
+                            : styles.errorMessage
+                    }
+                >
+                    {validationMessage.message}
+                </div>
+            )}
+        </div>
     );
 }
